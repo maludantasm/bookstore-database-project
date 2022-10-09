8a160de8643f328d6bbbae0ff8c9c9528745cf51
Belchior Silva <belchiorisv@gmail.com>
2022-10-09 18:25:02 -0300

    Change tables

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
--- Saber quantos funcionários existem por filial com mais de 3 funcionarios
-SELECT Nome, COUNT(*) FROM Filial F
-    INNER JOIN Trabalha T ON F.Id = T.FilialId
+-- Saber quantos funcionários existem por seção com mais de 3 funcionarios
+SELECT Nome, COUNT(*) FROM Secao S
+    INNER JOIN Trabalha T ON S.Id = T.SecaoId
     GROUP BY Nome
     HAVING COUNT(*) > 3;
