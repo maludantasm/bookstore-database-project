a4636f3a8e89ae1bce0382c9592d0eea0cd461d8
Belchior Silva <belchiorisv@gmail.com>
2022-10-10 14:50:31 -0300

    Fixes and additions

--- queries2.sql
+++ queries2.sql
@@ -3,3 +3,34 @@ SELECT Nome, COUNT(*) FROM Secao S
     INNER JOIN Trabalha T ON S.Id = T.SecaoId
     GROUP BY Nome
     HAVING COUNT(*) > 3;
+
+-- Saber, a partir do CPF, quais livros uma pessoa comprou
+SELECT Titulo FROM Livro L WHERE ISBN IN (
+    SELECT ISBN
+    FROM Compra
+    WHERE CPF = '20981209076'
+);
+
+-- Listar a quantidade de livros por categoria
+SELECT C.Nome, COUNT(*) FROM Categoria C
+    INNER JOIN Livro L
+    ON C.Id = L.CategoriaId
+    GROUP BY C.Nome;
+
+-- Listar o ISBN dos livros que foram comprados
+SELECT L.Titulo FROM Livro L WHERE L.ISBN IN (
+    SELECT ISBN FROM Compra
+);
+
+SELECT L.Titulo FROM Livro L WHERE EXISTS (
+    SELECT * FROM Compra C WHERE C.ISBN = L.ISBN
+);
+
+-- Listar o ISBN dos livros que não foram comprados
+SELECT L.Titulo FROM Livro L WHERE L.ISBN NOT IN (
+    SELECT ISBN FROM Compra
+);
+
+SELECT L.Titulo FROM Livro L WHERE NOT EXISTS (
+    SELECT * FROM Compra C WHERE C.ISBN = L.ISBN
+);
